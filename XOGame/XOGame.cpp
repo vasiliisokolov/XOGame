49a1ea84a9081599623f3961f1e36f4a4045fc58
Vasily Sokolov <vasilysokolov@mail.ru>
2022-08-15 15:56:44 +0300

    4 FUNCTION

--- XOGame/XOGame.cpp
+++ XOGame/XOGame.cpp
@@ -2,11 +2,13 @@
 
 bool check_format(std::string);
 char won_in_line(std::string);
-char won_in_column(std::string, std::string, std::string);
+std::string get_column(std::string, std::string, std::string, int);
+char search_one_element(std::string, std::string, std::string, int, int);
 
 int main()
 {
     std::string line0, line1, line2;
+    std::string column0, column1, column2;
     std::cout << "Lets play Noughts and Crosses!\n";
     while (check_format(line0) || check_format(line1) || check_format(line2))
     {
@@ -15,11 +17,19 @@ int main()
         std::cin >> line1;
         std::cin >> line2;
     }
-    
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << search_one_element(line0, line1, line2, x, y);
+    //column0 = get_column(line0, line1, line2, 0);
+    //column1 = get_column(line0, line1, line2, 1);
+    //column2 = get_column(line0, line1, line2, 2);
 
-    std::cout << "Line0: " << won_in_line(line0) << std::endl;
-    std::cout << "Line1: " << won_in_line(line1) << std::endl;
-    std::cout << "Line2: " << won_in_line(line2) << std::endl;
+    //std::cout << "Line0: " << won_in_line(line0) << std::endl;
+    //std::cout << "Line1: " << won_in_line(line1) << std::endl;
+    //std::cout << "Line2: " << won_in_line(line2) << std::endl;
+    //std::cout << "column0: " << won_in_line(column0) << std::endl;
+    //std::cout << "column1: " << won_in_line(column1) << std::endl;
+    //std::cout << "column2: " << won_in_line(column2) << std::endl;
 }
 
 bool check_format(std::string line)
@@ -74,4 +84,44 @@ char won_in_line(std::string line)
         return '.';
     }
     else return 'N';
+}
+
+std::string get_column(std::string line0, std::string line1, std::string line2, int columnNumber)
+{
+    std::string result = "";
+    result += line0[columnNumber];
+    result += line1[columnNumber];
+    result += line2[columnNumber];
+    return result;
+}
+
+char search_one_element(std::string line0, std::string line1, std::string line2, int x, int y)
+{
+    std::string line;
+    switch (x)
+    {
+    case 0:
+        line = line0;
+        break;
+    case 1:
+        line = line1;
+        break;
+    case 2:
+        line = line2;
+        break;
+    }
+    char result;
+    switch (y)
+    {
+    case 0:
+        result = line[0];
+        break;
+    case 1:
+        result = line[1];
+        break;
+    case 2:
+        result = line[2];
+        break;
+    }
+    return result;
 }
